93e694a8328f10cf497595ce81dcb115cb41b70a
ca-m <deconnick.carmel@gmail.com>
2023-04-24 14:28:10 +0200

    mandatory files

---
+++ 3-quick_sort.c
@@ -0,0 +1,90 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - swap two integers in array
+ * @a: first integer t swap
+ * @b: second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/**
+ * lomuto_partition - order subset of an array of integers according
+ *                    to lomuto partition scheme
+ *@array:array of integers
+ * @size: size of array
+ *@left:starting index of subset to order
+ * @right:ending index of subset to order
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+    int *pivot, above, below;
+
+    pivot = array +right;
+    for (above = below = left; below < right; below++)
+    {
+        if(array[below] <*pivot)
+        {
+            if (above < below)
+            {
+                swap_ints(array + below, array + above);
+                print_array(array, size);
+            }
+            above++;
+        }
+    }
+
+    if (array[above] > *pivot)
+    {
+        swap_ints(array + above, pivot);
+        print_array(array, size);
+    }
+
+    return (above);
+}
+
+/**
+ * lomuto_sort - implement quicksort algorithm through recursion
+ * @array: array of integers to sort
+ * @size: size of array
+ * @left: starting index of array partition to order
+ * @right: ending index of array petition to order
+ * description: uses lomuto partition scheme
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+    int part;
+
+    if (right - left > 0)
+    {
+        part = lomuto_partition(array, size, left, right);
+        lomuto_sort(array, size, left, part - 1);
+        lomuto_sort(array, size, part + 1, right)
+    }
+}
+
+/**
+ * quick_sort -sort an array of integers in ascending order
+ *              using quicksort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * description: uses lomuto partition scheme and prints array
+ *              after each swap of two elements
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    lomuto_sort(array, size, 0, size - 1);
+}
---
+++ deck.h
@@ -0,0 +1,50 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdlib.h>
+
+/**
+ * enum kind_e -enumeration of card suits
+ * @SPADE: spades suit
+ * @HEART: hearts suit
+ * @CLUB: clubs suit
+ * @DIAMOND: diamonds suit
+ */
+
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - playing card
+ * @value: value of the card, Ace to King
+ * @kind:kind of card
+ */
+
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node-s - deck of card
+ * @card:pointer to card of node
+ * @prev: pointer to previous node of list
+ * @next: pointer to next node of list
+ */
+
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+
+#endif/*DECK_H*/
